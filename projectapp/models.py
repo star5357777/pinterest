0142a3c0f92d6031df225bc74b4b0dab29695bf1
krystal <star535@nate.com>
2023-04-11 13:33:18 +0900

    projectapp 시작 및 초기 구현

---
+++ projectapp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Project(models.Model):
+    image = models.ImageField(upload_to='project/', null=False)
+    title = models.CharField(max_length=100, null=False)
+    description = models.TextField(max_length=100, null=True)
+    created_at = models.DateTimeField(auto_now=True)
