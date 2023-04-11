4c2f91f24af80c1f845c831f1ed1942c931daf9c
krystal <star535@nate.com>
2023-04-11 13:56:13 +0900

    projectapp 마무리

--- articleapp/models.py
+++ articleapp/models.py
@@ -1,10 +1,14 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+from projectapp.models import Project
+
+
 # Create your models here.
 
 class Article(models.Model):
     writer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='article')
+    project =models.ForeignKey(Project, on_delete=models.SET_NULL, null=True, related_name='article')
 
     title = models.CharField(max_length=200, null=True)
     image = models.ImageField(upload_to='article/', null=False)
